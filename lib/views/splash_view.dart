18901f8cf5b2dec691beeec7fb342498893f9677
Manash Kumar <manashkumar1216@gmail.com>
2022-12-14 14:57:53 +0530

    added_launcher_icon

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:prastuti_23/config/screen_config.dart';
+import 'package:prastuti_23/utils/routes/route_names.dart';
 import 'package:prastuti_23/view_models/splash_view_model.dart';
-
+import 'package:prastuti_23/views/authentication/login_view.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -21,20 +23,81 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   void initState() {
-    //Comment This line .. if u wanna stay in the splash screen..
-    splashViewModel.initiateApp(context);
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async{
+    await Future.delayed(Duration(milliseconds: 5500), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginView())
+    );
   }
 
 
   //TODO: Complete the UI of Splash Screen - Manash/Yash
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          VideoWidget(),
 
-      body: Center(
-        child: Text("This is Splash Screen")
+        ],
       ),
-
     );
+  }
+}
+
+class VideoWidget extends StatefulWidget {
+  const VideoWidget({Key? key}) : super(key: key);
+
+  @override
+  _VideoWidgetState createState() => _VideoWidgetState();
+}
+
+class _VideoWidgetState extends State<VideoWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/videos/splash_screen_video_mute.mp4')
+      ..initialize().then((_) {
+        _controller.setVolume(0.0);
+        _controller.setLooping(true);
+        _controller.play();
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: _controller.value.isInitialized
+          ? LayoutBuilder(
+        builder: (context, constraints) {
+          return SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width:
+                constraints.maxWidth * _controller.value.aspectRatio,
+                height: constraints.maxHeight,
+                child: VideoPlayer(_controller),
+              ),
+            ),
+          );
+        },
+      )
+          : Container(),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
