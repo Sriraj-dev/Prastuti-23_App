98136f28db6b58e846e0c5e0d4b28bab56cfaa4e
Manash Kumar <manashkumar1216@gmail.com>
2022-12-15 04:25:28 +0530

    login_view

--- lib/view_models/login_view_model.dart
+++ lib/view_models/login_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:core';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -31,3 +33,21 @@ class LoginViewModelNotifier extends StateNotifier<bool>{
 
 
 }
+
+List<String> title =[
+  "About Us",
+  "Enthralling Events",
+  "Exciting Rewards"
+];
+
+List<String> detail = [
+  "Annual departmental fest of the department of Electrical Engineering, Prastuti provides curious minds a platform to lean, compete and develop their technical skills",
+  "The 21st iteration brings a series of 6 intellectual and innovative events with workshops and fun challenging problem statements",
+  "Tempeting Prizes to win through different competitions, Access to Guest Lectures by expert in the field, Excellent learning opportunities"
+];
+
+List<String> images = [
+  "assets/login_view/sample_image_01.jpg",
+  "assets/login_view/sample_image_02.png",
+  "assets/login_view/sample_image_01.jpg",
+];
--- lib/views/authentication/login_view.dart
+++ lib/views/authentication/login_view.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:prastuti_23/view_models/login_view_model.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -17,36 +21,308 @@ class _LoginViewState extends State<LoginView> {
   // You can customise the Buttons(UI) as per your design
   //I will implement the OnTap/OnPressed Functions if u r not getting it..
 
+  int _currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Text("This is the Login Screen"),
-        ),
-      ),
-
-      //TODO: Just an Example to understand the flow
-      floatingActionButton: Consumer(
-        builder: (context, ref, child) {
-          bool isLoading = ref.watch(isLoggingIn);
-          return FloatingActionButton(
-            child: isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(
-                    color: Colors.white,
-                  ))
-                : const Center(
-                    child: Text("Login"),
+    return SafeArea(
+      child: Stack(
+        children: [
+          Positioned(
+            left: MediaQuery.of(context).size.width*0.2,
+            top: MediaQuery.of(context).size.height*0.025,
+            height: MediaQuery.of(context).size.height*0.08,
+            child: Container(
+              width: MediaQuery.of(context).size.width*0.6,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/login_view/prastuti'23_logo_1.png"),
+                      fit: BoxFit.fitWidth
+                  )
+              ),
+            )
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height*0.12, 0, MediaQuery.of(context).size.height*0.10),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                const AnimatedSwitcher(
+                  duration: Duration(milliseconds: 500),
+                ),
+                FractionallySizedBox(
+                  child: PageView.builder(
+                    itemCount: images.length,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    itemBuilder: (BuildContext context, int index) {
+                      return FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: Stack(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(0, 20, 0, 250),
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(images[index]),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.circular(32),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 10,
+                                    offset: Offset(0, 5),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Positioned(
+                              bottom: MediaQuery.of(context).size.height*0.03,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    title[index],
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      decoration: TextDecoration.none,
+                                      fontFamily: "Roboto",
+                                      fontSize: 40,
+                                    ),
+                                    textAlign: TextAlign.start,
+                                  ),
+                                  Container(
+                                    height: 20,
+                                  ),
+                                  Container(
+                                    height: MediaQuery.of(context).size.height*0.15,
+                                    width: MediaQuery.of(context).size.width*0.80,
+                                    child: Text(
+                                      detail[index],
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        decoration: TextDecoration.none,
+                                        fontFamily: "Roboto",
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.normal
+                                      ),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            )
+                          ]
+                        ),
+                      );
+                    },
                   ),
-            onPressed: () {
-              ref.read(isLoggingIn.notifier)
-                  .login(username: "", password: "", context: context);
-            },
-          );
-        },
-      ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: MediaQuery.of(context).size.width*0.05,
+            bottom: MediaQuery.of(context).size.height*0.05,
+            height: MediaQuery.of(context).size.height*0.05,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                AnimatedSmoothIndicator(
+                  activeIndex: _currentPage,
+                  count: 3,
+                  effect: WormEffect(
+                    activeDotColor: Color(0xff272727),
+                    dotHeight: 5.0
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width*0.43,
+                ),
+                ElevatedButton(
+                  // OnPressed to be implemented
+                  onPressed: () {
+                    // TODO: Google Login to be implemented by Siraj bhaiya
+                  },
+                  child: Container(
+                    padding: EdgeInsets.only(right: 20),
+                    height: 50,
+                    child: Image.asset('assets/login_view/google.png'),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: StadiumBorder(),
+                    backgroundColor: Colors.transparent,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //
+  //   return SafeArea(
+  //     child: Scaffold(
+  //       body: Stack(
+  //         children: [
+  //           LiquidSwipe(
+  //             pages: [
+  //               Container(
+  //                 width: MediaQuery.of(context).size.width,
+  //                 color: Color.fromARGB(26, 13, 27, 196),
+  //                 child: Column(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //                   children: [
+  //                     Image.asset('assets/login_view/sample_image_01.jpg'),
+  //                     Column(
+  //                       children: [
+  //                         Text(
+  //                           'About Us',
+  //                           style: TextStyle(
+  //                             fontSize: 45,
+  //                             fontWeight: FontWeight.bold,
+  //                             color: Color.fromARGB(255, 13, 27, 196),
+  //                             fontFamily: 'Roboto',
+  //                             shadows: [
+  //                               Shadow(
+  //                                 offset: Offset(2.0, 2.0),
+  //                                 blurRadius: 3.0,
+  //                                 color: Color.fromARGB(129, 13, 27, 196),
+  //                               ),
+  //                             ],
+  //                           ),
+  //                         ),
+  //                         Text(aboutUs),
+  //                       ],
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //               Container(
+  //                 width: MediaQuery.of(context).size.width,
+  //                 color: Colors.white,
+  //                 child: Column(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //                   children: [
+  //                     Image.asset('assets/login_view/sample_image_02.png'),
+  //                     Column(
+  //                       children: [
+  //                         Text('Hi'),
+  //                         Text('Hi'),
+  //                       ],
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //               Container(
+  //                 width: MediaQuery.of(context).size.width,
+  //                 color: Colors.white,
+  //                 child: Column(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //                   children: [
+  //                     Image.asset('assets/login_view/sample_image_01.jpg'),
+  //                     Column(
+  //                       children: [
+  //                         Text('Hi'),
+  //                         Text('Hi'),
+  //                       ],
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //               Container(
+  //                 width: MediaQuery.of(context).size.width,
+  //                 color: Colors.white,
+  //                 child: Column(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //                   children: [
+  //                     Image.asset('assets/login_view/sample_image_02.png'),
+  //                     Column(
+  //                       children: [
+  //                         Text('Hi'),
+  //                         Text('Hi'),
+  //                       ],
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ],
+  //             onPageChangeCallback: onPageChangedCallBack,
+  //             slideIconWidget: const Icon(Icons.arrow_back_ios),
+  //             enableSideReveal: true,
+  //           ),
+  //           Positioned(
+  //               left: MediaQuery.of(context).size.width*0.2,
+  //               top: MediaQuery.of(context).size.height*0.025,
+  //               height: MediaQuery.of(context).size.height*0.08,
+  //               child: Container(
+  //                 width: MediaQuery.of(context).size.width*0.6,
+  //                 decoration: BoxDecoration(
+  //                     image: DecorationImage(
+  //                         image: AssetImage("assets/login_view/prastuti'23_logo_1.png"),
+  //                         fit: BoxFit.fitWidth
+  //                     )
+  //                 ),
+  //               )
+  //           ),
+  //           Positioned(
+  //             left: MediaQuery.of(context).size.width*0.05,
+  //             bottom: MediaQuery.of(context).size.height*0.03,
+  //             height: MediaQuery.of(context).size.height*0.1,
+  //             child: Container(
+  //               width: MediaQuery.of(context).size.width*0.9,
+  //               child: Row(
+  //                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //                 children: [
+  //                   AnimatedSmoothIndicator(
+  //                     activeIndex: controller.currentPage,
+  //                     count: 3,
+  //                     effect: WormEffect(
+  //                       activeDotColor: Color(0xff272727),
+  //                       dotHeight: 5.0
+  //                     ),
+  //                   ),
+  //                   Container(
+  //                     width: MediaQuery.of(context).size.width*0.2,
+  //                   ),
+  //                   ElevatedButton(
+  //                     // OnPressed to be implemented
+  //                     onPressed: () {},
+  //                     child: Container(
+  //                       height: 50,
+  //                       child: Column(
+  //                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //                         children: [
+  //                           Text(
+  //                             'Google Login',
+  //                             style: TextStyle(
+  //                               fontWeight: FontWeight.bold,
+  //                               fontSize: 20,
+  //                             ),
+  //                           ),
+  //                         ],
+  //                       ),
+  //                     )
+  //                   )
+  //                 ],
+  //               ),
+  //             )
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
+
+
