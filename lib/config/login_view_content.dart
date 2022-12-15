eef17c58513a7ed68ea05416dfa832d141aad0ef
Manash Kumar <manashkumar1216@gmail.com>
2022-12-15 23:42:22 +0530

    ImprovedUi

---
+++ lib/config/login_view_content.dart
@@ -0,0 +1,18 @@
+
+List<String> title =[
+  "About Us",
+  "Enthralling Events",
+  "Exciting Rewards"
+];
+
+List<String> detail = [
+  "Annual departmental fest of the department of Electrical Engineering, Prastuti provides curious minds a platform to lean, compete and develop their technical skills",
+  "The 21st iteration brings a series of 6 intellectual and innovative events with workshops and fun challenging problem statements",
+  "Tempeting Prizes to win through different competitions, Access to Guest Lectures by expert in the field, Excellent learning opportunities"
+];
+
+List<String> images = [
+  "assets/login_view/awards.png",
+  "assets/login_view/events.png",
+  "assets/login_view/awards.png",
+];
