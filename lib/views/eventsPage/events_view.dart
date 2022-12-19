4990e392ed103ba9087a8c59c80f34436c110954
Sriraj-dev <Psriraj1902@gmail.com>
2022-12-19 13:17:35 +0530

    Changes in Event_Ui

--- lib/views/eventsPage/events_view.dart
+++ lib/views/eventsPage/events_view.dart
@@ -1,12 +1,9 @@
-
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:prastuti_23/animations/events_view_animation.dart';
 import 'package:prastuti_23/config/color_palette.dart';
 import 'package:prastuti_23/config/screen_config.dart';
-import 'package:prastuti_23/views/eventsPage/events.dart';
+import 'package:prastuti_23/views/eventsPage/events_view_content.dart';
 
 class EventsView extends StatefulWidget {
   const EventsView({Key? key}) : super(key: key);
@@ -38,196 +35,117 @@ class _EventsViewState extends State<EventsView> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        // backgroundColor: AppTheme().backgroundColor,
-        backgroundColor: Colors.purple,
-        body: Padding(
-          padding: const EdgeInsets.only(bottom: 20,top: 20,left: 0,right: 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Obx(() => Opacity(
-                        opacity: animationController.pagePaddingValue.value,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: (1 - animationController
-                                          .pagePaddingValue.value) * 30),
-                          child: Text(
-                            titles[_selectedEvent.value],
-                            style: TextStyle(
-                                fontSize: 45,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                          ),
+    return Scaffold(
+      backgroundColor: AppTheme().backgroundColor,
+
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 0,top: 50,left: 0,right: 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Obx(() => Opacity(
+                      opacity: animationController.pagePaddingValue.value,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: (1 - animationController
+                                        .pagePaddingValue.value) * 30),
+                        child: Text(
+                          titles[_selectedEvent.value],
+                          style: TextStyle(
+                              fontSize: 55,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
                         ),
-                      ) ,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Obx(
-                      () => Opacity(
-                        opacity: animationController.pagePaddingValue.value,
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                              left: (1 -
-                                      animationController
-                                          .pagePaddingValue.value) *
-                                  30),
-                          child: Text(
-                            desc[_selectedEvent.value],
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.black,
-                              ),
-                          ),
+                      ),
+                    ) ,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Obx(
+                    () => Opacity(
+                      opacity: animationController.pagePaddingValue.value,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: (1 -
+                                    animationController
+                                        .pagePaddingValue.value) *
+                                30),
+                        child: Text(
+                          desc[_selectedEvent.value],
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              SizedBox(
+            ),
+            Container(
                 height: SizeConfig.heightPercent * 25,
                 child: PageView.builder(
-                    controller: PageController(viewportFraction: 0.9),
+                    controller: PageController(viewportFraction: 0.88),
                     itemCount: titles.length,
                     onPageChanged: ((value) {
                       _selectedEvent.value = value;
                       eventsViewAnimation.restartPageAnimation();
                     }),
                     itemBuilder: (context, index) {
-                      return event_tab(index);
+                      return event_image(index);
                     }),
-                )
-            ],
-          ),
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home),label: "home"),
-            BottomNavigationBarItem(icon: Icon(Icons.person),label: "profile"),
-            BottomNavigationBarItem(icon: Icon(Icons.settings),label: "settings"),
+              )
           ],
         ),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppTheme().backgroundColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: "profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings),label: "settings"),
+        ],
       ),
     );
   }
 
-  Widget event_tab(int index){
-    return Card(
-      child: Container(
-        padding: EdgeInsets.only(left: 20),
-        height: SizeConfig.heightPercent * 25,
-        width: SizeConfig.width - 20,
-        margin: const EdgeInsets.only(right: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          image: DecorationImage(
-            image: AssetImage(event_images[index]),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 18),
-              height: SizeConfig.heightPercent*10,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Card_Text(titles[index], 35, 10, 0),
-                  Card_Text('Schedule', 20, 20, 0)
-                ],
+  Widget event_image(int index){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30),
+        elevation: 0,
+        child: Container(
+          padding: EdgeInsets.only(left: 20),
+          height: SizeConfig.heightPercent * 25,
+          width: SizeConfig.width - 20,
+          //margin: const EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            //TODO: decide shadow color .
+            boxShadow: [
+              BoxShadow(
+                color: AppTheme().secondaryColor.withOpacity(0.5),
+                spreadRadius: 6,
+                blurRadius: 10,
+                offset: Offset(0, 5),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Container(
-                //   padding: EdgeInsets.only(right: 25),
-                //   child: Row(
-                //     mainAxisAlignment: MainAxisAlignment.end,
-                //     children: [
-                //       Card_Button('Submit', BtnColor().submitBtn),
-                //     ],
-                //   ),
-                // ),
-                Container(
-                  padding: EdgeInsets.only(right: 25, bottom: 19),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Card_Button('Register', BtnColor().regBtn)
-                    ],
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-      elevation: 10.0,
-      color: Colors.transparent,
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.circular(30)
-      ),
-      shadowColor: Colors.purple.shade900,
-    );
-  }
-
-}
-
-Widget Card_Text (String text, double size, double left, double top) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: [
-      Container(
-        padding: EdgeInsets.fromLTRB(left, top, 0, 0),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: size,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Roboto',
-            color: TextColour().white,
-            letterSpacing: 1.5,
+            ],
+            image: DecorationImage(
+                image: AssetImage(event_images[index]),
+                fit: BoxFit.cover),
           ),
         ),
       ),
-    ],
-  );
-}
+    );
+  }
 
-Widget Card_Button (String text, Color color) {
-  return ElevatedButton(
-    onPressed: () {
-      // TODO: Submission Link
-    },
-    child: Text(
-      text,
-      style: TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.bold,
-        fontFamily: 'Roboto',
-        color: TextColour().white,
-        letterSpacing: 1,
-      ),
-    ),
-    style: ElevatedButton.styleFrom(
-      shape: const StadiumBorder(),
-      backgroundColor: Color.fromARGB(255, 4, 106, 150),
-      fixedSize: Size(130, 40),
-      shadowColor: Color.fromARGB(255, 0, 76, 125),
-      elevation: 15,
-    ),
-  );
 }
