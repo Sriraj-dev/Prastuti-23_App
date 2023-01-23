8f4d345e6cc9dc9194c436b80f9f188d54cce46c
Manash Kumar <manashkumar1216@gmail.com>
2023-01-23 18:02:47 +0530

    minor_fixes

--- lib/views/authentication/login_view.dart
+++ lib/views/authentication/login_view.dart
@@ -292,10 +292,10 @@ class _LoginViewState extends State<LoginView> with TickerProviderStateMixin {
           ),
           style: ElevatedButton.styleFrom(
             shape: const StadiumBorder(),
-                  backgroundColor: AppTheme().primaryColorDark,
-                  fixedSize: Size(140.sp, 50.sp),
-                  shadowColor: AppTheme().primaryColorExtraDark,
-                  elevation: 0.sp,
+            backgroundColor: AppTheme().primaryColorDark,
+            fixedSize: Size(140.sp, 50.sp),
+            shadowColor: AppTheme().primaryColorExtraDark,
+            elevation: 10.sp,
           ),
         ): ElevatedButton(
           onPressed: () async {
@@ -352,7 +352,7 @@ class _LoginViewState extends State<LoginView> with TickerProviderStateMixin {
             backgroundColor: AppTheme().primaryColorDark,
             fixedSize: Size(140.sp, 50.sp),
             shadowColor: AppTheme().primaryColorExtraDark,
-            elevation: 15.sp,
+            elevation: 10.sp,
           ),
         )
     );
